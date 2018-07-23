7f931c37e646ff94c3f65e1bc9aa3a58d09cc3c5
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-23 20:02:03 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,7 +16,8 @@ def shipping_manifest
   }
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-  shipping_manifest
+end
+shipping_manifest
 end
 
 def retrieval
